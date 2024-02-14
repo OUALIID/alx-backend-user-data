5c2e1e9979f1b160b606073303380bf93526d8d3
OUALIID <walidelhadim8@gmail.com>
2024-02-14 09:08:51 +0100

    “Function

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,52 +2,41 @@
 """
 Route module for the API
 """
+from os import getenv
+from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
-import os
-from api.v1.views import app_views
-from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.auth import Auth
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-auth_type = os.environ.get("AUTH_TYPE", "auth")
 
-
-if auth_type == "basic_auth":
-    auth = BasicAuth()
-else:
+if getenv("AUTH_TYPE") == "AUTH_TYPE_1":
     auth = Auth()
 
 @app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler """
+def not_found(error):
     return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
-def Unauthorized(error) -> str:
-    """ Unauthorized access error handler """
+def Unauthorized(error):
     return jsonify({"error": "Unauthorized"}), 401
 
 @app.errorhandler(403)
-def Forbidden(error) -> str:
-    """ Access forbidden error handler """
+def Forbidden(error):
     return jsonify({"error": "Forbidden"}), 403
 
 @app.before_request
-def requestFilter() -> None:
-    
+def before_request():
     if auth is None:
         return
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-    ]
-    if not auth.require_auth(request.path, excluded_paths):
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if request.path in excluded_paths:
         return
     if auth.authorization_header(request) is None:
         abort(401)
@@ -55,6 +44,6 @@ def requestFilter() -> None:
         abort(403)
 
 if __name__ == "__main__":
-    host = os.getenv("API_HOST", "0.0.0.0")
-    port = os.getenv("API_PORT", "5001")
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5001")
     app.run(host=host, port=port)
