714f279ced36f9abe1e31575195b16d629ef3554
OUALIID <walidelhadim8@gmail.com>
2024-02-14 09:55:25 +0100

    Trying to restore a previous version

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,7 +8,6 @@ from flask import Flask, jsonify, abort, request, abort
 from flask_cors import CORS, cross_origin
 import os
 from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
