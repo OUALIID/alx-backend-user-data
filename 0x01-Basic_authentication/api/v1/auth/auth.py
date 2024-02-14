b52206f32934a830be8d818f6b5bb64492619c80
OUALIID <walidelhadim8@gmail.com>
2024-02-14 04:02:34 +0100

    Update the method def require_auth(self, path: str, excluded_paths: List[str]) -> bool: in Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,14 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if authentication is required for a given path"""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        
+        for excluded_path in excluded_paths:
+            if path.rstrip('/') == excluded_path.rstrip('/'):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Get the authorization header from the request"""
