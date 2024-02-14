5fdc2051961505774f71a916c5bebbf00784e8cf
OUALIID <walidelhadim8@gmail.com>
2024-02-14 04:01:38 +0100

    Update the method def require_auth(self, path: str, excluded_paths: List[str]) -> bool: in Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,13 +21,3 @@ class Auth:
         """Get the current user from the request"""
         return None
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ """
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
-            return True
-
-        for excluded_path in excluded_paths:
-            if path.rstrip('/') == excluded_path.rstrip('/'):
-                return False
-
-        return True
