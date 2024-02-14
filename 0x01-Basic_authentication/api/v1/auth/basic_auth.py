ddde11eb666c8f2df4bb73275a8c9d320e5c599a
OUALIID <walidelhadim8@gmail.com>
2024-02-14 10:33:43 +0100

    fix my code

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth class that inherits from Auth."""
-    pass
+    def extract_base64_authorization_header(
+        self, authorization_header: str) -> str:
+        """A method that returns the Base64 of the Authorization header."""
+        if authorization_header is None or not isinstance(
+            authorization_header, str):
+            return None
+        
+        if authorization_header.startswith("Basic "):
+            return authorization_header[6:]
+        else:
+            return None
+
