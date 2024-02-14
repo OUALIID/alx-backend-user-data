c33fbe44b3ef934db30bcf2f7163ef5c8aabeac1
OUALIID <walidelhadim8@gmail.com>
2024-02-14 10:40:47 +0100

    fix pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,11 +7,12 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth class that inherits from Auth."""
+
     def extract_base64_authorization_header(
-        self, authorization_header: str) -> str:
+            self, authorization_header: str) -> str:
         """A method that returns the Base64 of the Authorization header."""
-        if authorization_header is None or not isinstance(
-            authorization_header, str):
+        if (authorization_header is None or
+                not isinstance(authorization_header, str)):
             return None
 
         if authorization_header.startswith("Basic "):
