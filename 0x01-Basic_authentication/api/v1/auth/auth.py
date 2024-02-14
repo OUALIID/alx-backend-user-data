6c72b368eb9d96ee4d98d738d37bf5e3b6e94808
OUALIID <walidelhadim8@gmail.com>
2024-02-14 09:38:02 +0100

    fix my code

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -19,11 +19,10 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """Get the authorization header from the request"""
-        return (
-            None if not request or "Authorization" not in request.headers
-            else request.headers["Authorization"]
-        )
+        """doc doc doc"""
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Get the current user from the request"""
