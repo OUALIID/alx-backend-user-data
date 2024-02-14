8cbfb7e03fd8225fdf3acbb44598fa2fb0097620
OUALIID <walidelhadim8@gmail.com>
2024-02-14 04:00:48 +0100

    Update the method def require_auth(self, path: str, excluded_paths: List[str]) -> bool: in Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,7 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ """
-        if path and excluded_paths is None or len(excluded_paths) == 0:
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
 
         for excluded_path in excluded_paths:
