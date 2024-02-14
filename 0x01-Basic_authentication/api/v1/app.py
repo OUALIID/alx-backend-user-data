3c20ee4c751b1460c9802adea7ccb1ac7d932d47
OUALIID <walidelhadim8@gmail.com>
2024-02-14 08:35:46 +0100

    trying to fix my code

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -44,18 +44,22 @@ def Forbidden(error) -> str:
 
 
 @app.before_request
-def before_request():
+def before_request() -> None:
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if request.path not in excluded_paths:
-        if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
-                abort(401)
-            if auth.current_user(request) is None:
-                abort(403)
+    if not auth.require_auth(
+        request.path,
+        [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+        ],
+    ):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
