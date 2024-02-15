64d53d11840c0a03f5326588394be5c8c5c9b755
OUALIID <walidelhadim8@gmail.com>
2024-02-15 08:44:50 +0100

    Extract user email and password from decrypted Base64 authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 API Authentication Module
 """
 from api.v1.auth.auth import Auth
+from typing import Tuple
 import base64
 
 
@@ -32,3 +33,12 @@ class BasicAuth(Auth):
                 base64_authorization_header).decode('utf-8')
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """Extract user email and password from decoded Base64 authorization header."""
+        if (not isinstance(decoded_base64_authorization_header, str) or ':' not in 
+                decoded_base64_authorization_header):
+            return None, None
+        email, password = decoded_base64_authorization_header.split(":", 1)
+        return email, password
