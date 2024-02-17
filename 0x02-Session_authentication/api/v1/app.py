668016c960e9db7fc7493b14d6c9ad6219216c4c
OUALIID <walidelhadim8@gmail.com>
2024-02-17 07:30:07 +0100

    Added logic to abort with 401 error if both authorization header and session cookie are None

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -27,13 +27,16 @@ else:
 
 @app.before_request
 def before_request_func():
-    """doc doc doc"""
+    """Function to be called before each request. """
     if auth is None:
         return
     if not auth.require_auth(request.path, ['/api/v1/status/',
                                             '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
+                                            '/api/v1/forbidden/',
+                                            '/api/v1/auth_session/login/']):
         return
+    if auth.session_cookie(request) is None:
+        abort(401)
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
