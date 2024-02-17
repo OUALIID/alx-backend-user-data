05b32fa0590d211c3e4d256a4da8e31123b18899
OUALIID <walidelhadim8@gmail.com>
2024-02-17 06:20:43 +0100

    def instance method user_id_for_session_id(self, session_id: str = None) -> str: returns user id based on session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,7 +20,7 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ Returns the user ID associated with the provided session ID."""
+        """Returns the user ID associated with the provided session ID."""
         if session_id is None or not isinstance(session_id, str):
             return None
 
