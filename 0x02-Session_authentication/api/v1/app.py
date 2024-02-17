d08fa02cd7b9be192858dde6abd2c6c045e01551
OUALIID <walidelhadim8@gmail.com>
2024-02-17 04:57:16 +0100

    Updated api/v1/app.py to use the SessionAuth instance of the auth variable based on the value of the environment variable AUTH_TYPE, if AUTH_TYPE equals session_auth.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import CORS, cross_origin
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -18,9 +19,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 if os.getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
-elif os.getenv("AUTH_TYPE") == "auth":
+elif os.getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
+else:
     auth = Auth()
-
 
 @app.before_request
 def before_request_func():
