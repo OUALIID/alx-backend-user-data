17212bb9c68c0744f795329fbc1aace72026c6e0
OUALIID <walidelhadim8@gmail.com>
2024-02-18 12:30:02 +0100

    try to fix 2 err

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -12,10 +12,3 @@ class UserSession(Base):
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
-
-    """@classmethod
-    def create(cls, user_id, session_id):
-        Create a new UserSession instance and save it to the database
-        new_session = cls(user_id=user_id, session_id=session_id)
-        new_session.save()
-        return new_session"""
