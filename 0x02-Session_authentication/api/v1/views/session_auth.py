c8b5424ba1343e492052317ca65c8e2aa23ea942
OUALIID <walidelhadim8@gmail.com>
2024-02-18 05:13:22 +0100

    try to fix my code 7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -35,10 +35,9 @@ def login():
     return response
 
 
-@app_views.route(
-    '/api/v1/auth_session/logout',
-    methods=['DELETE'],
-    strict_slashes=False)
+@app_views.route("/auth_session/logout",
+                methods=["DELETE"],
+                strict_slashes=False)
 def logout():
     """Logout route to delete the session."""
     from api.v1.app import auth
