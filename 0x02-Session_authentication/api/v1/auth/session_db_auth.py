c80764dc043b0b7541535630610d6cf6656ed0e1
OUALIID <walidelhadim8@gmail.com>
2024-02-18 14:26:43 +0100

    Fix all problems

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -10,6 +10,13 @@ from os import getenv
 class SessionDBAuth(SessionExpAuth):
     """ Provides session ID authentication using a database."""
 
+    def __init__(self):
+            super().__init__()
+            try:
+                self.session_duration = int(getenv("SESSION_DURATION"))
+            except (TypeError, ValueError):
+                self.session_duration = 0
+
     def create_session(self, user_id=None):
         """Creates a new UserSession instance and returns the session ID."""
         session_id = super().create_session(user_id)
@@ -21,6 +28,11 @@ class SessionDBAuth(SessionExpAuth):
 
     def user_id_for_session_id(self, session_id=None):
         """Returns the associated user ID for the given session ID."""
+        try:
+            self.session_duration = int(getenv("SESSION_DURATION"))
+        except (TypeError, ValueError):
+            self.session_duration = 0
+
         if session_id and isinstance(session_id, str):
             return super().user_id_for_session_id(session_id)
 
