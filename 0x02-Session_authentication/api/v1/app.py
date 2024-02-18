4918ae2ffe9a97ef7570419fb90b555862db97eb
OUALIID <walidelhadim8@gmail.com>
2024-02-18 09:57:20 +0100

    try to fix my code 6

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
@@ -26,7 +27,6 @@ elif os.getenv("AUTH_TYPE") == "session_auth":
 elif os.getenv("AUTH_TYPE") == "session_exp_auth":
     auth = SessionExpAuth()
 elif os.getenv("AUTH_TYPE") == "SessionDBAuth":
-    from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
 else:
     auth = Auth()
--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -8,7 +8,8 @@ from models.user_session import UserSession
 
 class SessionDBAuth(SessionExpAuth):
     """ Provides session ID authentication using a database."""
-    def create_session(self, user_id=None):
+
+    def create_session(self, user_id=None) -> str:
         """Creates a new UserSession instance and returns the session ID."""
         session_id = super().create_session(user_id)
         if isinstance(session_id, str):
@@ -16,14 +17,14 @@ class SessionDBAuth(SessionExpAuth):
             return session_id
         return None
 
-    def user_id_for_session_id(self, session_id=None):
+    def user_id_for_session_id(self, session_id=None) -> str:
         """Returns the associated user ID for the given session ID."""
         if session_id and isinstance(session_id, str):
             user_sessions = UserSession().search({"session_id": session_id})
             return user_sessions[0].user_id if user_sessions else None
         return None
 
-    def destroy_session(self, request=None):
+    def destroy_session(self, request=None) -> bool:
         """Destroys the session associated with the request cookie."""
         session_id = self.session_cookie(request)
         user_sessions = UserSession().search({"session_id": session_id})
