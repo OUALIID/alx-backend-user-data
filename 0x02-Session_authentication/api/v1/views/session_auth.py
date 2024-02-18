81bae2de7e532d5f7ebeff456684643e80e37347
OUALIID <walidelhadim8@gmail.com>
2024-02-18 01:47:36 +0100

    New Flask handles all session authentication paths.

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+"""
+from flask import Flask, request, jsonify
+from api.v1.views import app_views
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """ """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if email is None:
+        return jsonify({"error": "email missing"}), 400
+    if password is None:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({"email": email})
+
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    session_name = getenv("SESSION_NAME")
+    response.set_cookie(session_name, session_id)
+    return response
