b6746b119f50ed83cfb34e6e06b37613729541b1
OUALIID <walidelhadim8@gmail.com>
2024-02-18 12:19:53 +0100

    try to fix 2 err

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -13,7 +13,7 @@ class SessionDBAuth(SessionExpAuth):
         """Creates a new UserSession instance and returns the session ID."""
         session_id = super().create_session(user_id)
         if isinstance(session_id, str):
-            UserSession.create(user_id=user_id, session_id=session_id)
+            UserSession.create(user_id=user_id, session_id=session_id).save()
             return session_id
         return None
 
--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -13,9 +13,9 @@ class UserSession(Base):
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
 
-    @classmethod
+    """@classmethod
     def create(cls, user_id, session_id):
-        """Create a new UserSession instance and save it to the database."""
+        Create a new UserSession instance and save it to the database
         new_session = cls(user_id=user_id, session_id=session_id)
         new_session.save()
-        return new_session
+        return new_session"""
