967a71073a5e34fa850792defb4bcc7d97bd1aa8
OUALIID <walidelhadim8@gmail.com>
2024-02-18 14:40:21 +0100

    Fix all problems

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -5,17 +5,9 @@ SessionDBAuth module
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 from datetime import timedelta, datetime
-from os import getenv
 
 class SessionDBAuth(SessionExpAuth):
     """ Provides session ID authentication using a database."""
-
-    def __init__(self):
-            super().__init__()
-            try:
-                self.session_duration = int(getenv("SESSION_DURATION"))
-            except (TypeError, ValueError):
-                self.session_duration = 0
 
     def create_session(self, user_id=None):
         """Creates a new UserSession instance and returns the session ID."""
@@ -28,13 +20,17 @@ class SessionDBAuth(SessionExpAuth):
 
     def user_id_for_session_id(self, session_id=None):
         """Returns the associated user ID for the given session ID."""
-        try:
-            self.session_duration = int(getenv("SESSION_DURATION"))
-        except (TypeError, ValueError):
-            self.session_duration = 0
+        if not session_id and not isinstance(session_id, str):
+            return None
 
-        if session_id and isinstance(session_id, str):
-            return super().user_id_for_session_id(session_id)
+        instance = super().user_id_for_session_id(session_id)
+        if instance is None:
+            return None
+        try:
+            user_session = UserSession().search({"session_id": session_id})
+        except Exception:
+            return None
+        return user_session[0].user_id
 
     def destroy_session(self, request=None):
         """Destroys the session associated with the request cookie."""
