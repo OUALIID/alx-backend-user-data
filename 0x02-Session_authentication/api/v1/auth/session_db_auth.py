6ee515f217e40fb2df1b97b649a277a808f9da40
OUALIID <walidelhadim8@gmail.com>
2024-02-18 10:20:13 +0100

    try to fix my code 13

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,57 +1,34 @@
 #!/usr/bin/env python3
-'''authentication file'''
+"""
+SessionDBAuth module
+"""
 from api.v1.auth.session_exp_auth import SessionExpAuth
-from datetime import datetime, timedelta
 from models.user_session import UserSession
 
 
 class SessionDBAuth(SessionExpAuth):
-    '''SessionDBAuth class'''
+    """ Provides session ID authentication using a database."""
 
     def create_session(self, user_id=None):
-        '''create_session'''
+        """Creates a new UserSession instance and returns the session ID."""
         session_id = super().create_session(user_id)
-        if not session_id:
-            return None
-        sessionAuth = UserSession(
-            **{'user_id': user_id, 'session_id': session_id})
-        # sessionAuth.created_at = self.user_id_by_session_id.get(session_id)[
-        #     'created_at']
-        sessionAuth.save()
-        return session_id
+        if isinstance(session_id, str):
+            UserSession.create(user_id=user_id, session_id=session_id)
+            return session_id
+        return None
 
     def user_id_for_session_id(self, session_id=None):
-        '''user_id_for_session_id'''
-        if not session_id:
-            return None
-        session_dictionary = UserSession.search({'session_id': session_id})
-        # session_dictionary = UserSession().get(session_id)
-        if not session_dictionary:
-            return None
-        session_dictionary = session_dictionary[0]
-        user_id = session_dictionary.user_id
-        if self.session_duration <= 0:
-            return user_id
-        created_at = session_dictionary.created_at
-        if not created_at:
-            return None
-        session_Exp = (created_at) + timedelta(seconds=self.session_duration)
-        if session_Exp < datetime.utcnow():
-            return None
-        return user_id
+        """Returns the associated user ID for the given session ID."""
+        if session_id and isinstance(session_id, str):
+            user_sessions = UserSession().search({"session_id": session_id})
+            return user_sessions[0].user_id if user_sessions else None
+        return None
 
     def destroy_session(self, request=None):
-        '''methode that destroy session if there is one'''
-        if not request:
-            return False
+        """Destroys the session associated with the request cookie."""
         session_id = self.session_cookie(request)
-        if not session_id:
-            return False
-        if not self.user_id_for_session_id(session_id):
-            return False
-        session_dictionary = UserSession.search({"session_id": session_id})
-        if not session_dictionary:
-            return False
-        session_dictionary[0].remove()
-        UserSession.save_to_file()
-        return True
+        user_sessions = UserSession().search({"session_id": session_id})
+        if user_sessions:
+            user_sessions[0].delete()
+            return True
+        return False
