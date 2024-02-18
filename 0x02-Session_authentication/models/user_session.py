0b4211b02f9875a05eaa17a5f1d2d95c9b5bd7f0
OUALIID <walidelhadim8@gmail.com>
2024-02-18 09:15:26 +0100

    update the method __init__(self, *args: list, **kwargs: dict)

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -9,5 +9,5 @@ class UserSession(Base):
     def __init__(self, *args: list, **kwargs: dict):
         """ Initialize a new UserSession."""
         super().__init__(*args, **kwargs)
-        self.user_id = ""
-        self.session_id = ""
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
