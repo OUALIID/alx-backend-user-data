76ad926d2e9118beef75fc5e27a14210c56fdeb7
OUALIID <walidelhadim8@gmail.com>
2024-02-18 10:17:07 +0100

    try to fix my code 12

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """ user_session module
 """
-from .base import Base
+from models.base import Base
 
 
 class UserSession(Base):
     """ UserSession class representing a user session."""
+
     def __init__(self, *args: list, **kwargs: dict):
         """ Initialize a new UserSession."""
         super().__init__(*args, **kwargs)
-        self.user_id = kwargs.get('user_id')
-        self.session_id = kwargs.get('session_id')
+        self.user_id = ""
+        self.session_id = ""
