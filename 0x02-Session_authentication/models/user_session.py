c0dca385a29839286f29712ffdfbb539c0d6d546
OUALIID <walidelhadim8@gmail.com>
2024-02-18 10:38:56 +0100

    try to fix my code 14

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -10,5 +10,12 @@ class UserSession(Base):
     def __init__(self, *args: list, **kwargs: dict):
         """ Initialize a new UserSession."""
         super().__init__(*args, **kwargs)
-        self.user_id = ""
-        self.session_id = ""
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
+
+    @classmethod
+    def create(cls, user_id, session_id):
+        """Create a new UserSession instance and save it to the database."""
+        new_session = cls(user_id=user_id, session_id=session_id)
+        new_session.save()
+        return new_session
