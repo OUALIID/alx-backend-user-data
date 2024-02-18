91c82fa0eaad4122c01e41d8972d6d57c97af55e
OUALIID <walidelhadim8@gmail.com>
2024-02-18 09:58:50 +0100

    try to fix my code 7

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -9,22 +9,23 @@ from models.user_session import UserSession
 class SessionDBAuth(SessionExpAuth):
     """ Provides session ID authentication using a database."""
 
-    def create_session(self, user_id=None) -> str:
+    def create_session(self, user_id=None):
         """Creates a new UserSession instance and returns the session ID."""
         session_id = super().create_session(user_id)
         if isinstance(session_id, str):
-            UserSession.create(user_id=user_id, session_id=session_id)
-            return session_id
-        return None
+            user_session = UserSession(user_id=user_id, session_id=session_id)
+            user_session.save()
+            return (session_id)
+        return (None)
 
-    def user_id_for_session_id(self, session_id=None) -> str:
+    def user_id_for_session_id(self, session_id=None):
         """Returns the associated user ID for the given session ID."""
         if session_id and isinstance(session_id, str):
             user_sessions = UserSession().search({"session_id": session_id})
             return user_sessions[0].user_id if user_sessions else None
         return None
 
-    def destroy_session(self, request=None) -> bool:
+    def destroy_session(self, request=None):
         """Destroys the session associated with the request cookie."""
         session_id = self.session_cookie(request)
         user_sessions = UserSession().search({"session_id": session_id})
