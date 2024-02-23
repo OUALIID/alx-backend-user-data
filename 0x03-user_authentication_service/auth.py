5ab987563caec09088e69073f33625726fae9a9c
OUALIID <walidelhadim8@gmail.com>
2024-02-24 00:21:19 +0100

    Registers a new user in the authentication system.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,8 +2,25 @@
 """Hashes a password using bcrypt.
 """
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password using bcrypt."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user in the authentication system."""
+        if self._db.find_user_by(email=email):
+            raise ValueError
+        user = self._db.add_user(email, self._hash_password(password))
+        return user
