53d4ec33b0cc651b3e425f0acc5268c860722afc
OUALIID <walidelhadim8@gmail.com>
2024-02-23 02:07:50 +0100

    Try to fix my code

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,8 +10,9 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'users'
+    """SQLAlchemy model representing the 'users' table."""
 
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
