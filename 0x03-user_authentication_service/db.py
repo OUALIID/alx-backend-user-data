4aa237aa24c0fd2966de9c85dc6e2b4d04c3a832
OUALIID <walidelhadim8@gmail.com>
2024-02-23 17:16:30 +0100

    Find a user in the database based on provided query criteria.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -37,7 +37,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """Find a user in the database based on provided query criteria."""
         user = self._session.query(User).filter_by(**kwargs).first()
         if not user:
