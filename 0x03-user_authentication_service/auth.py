d251ad0a92ea00075ce68d18b1cd74f42f0c4485
OUALIID <walidelhadim8@gmail.com>
2024-02-23 23:48:26 +0100

    A method that takes the password string arguments and returns the bytes.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,5 +7,5 @@ import bcrypt
 
 def _hash_password(password= str) -> bytes:
     """Hashes a password using bcrypt."""
-    return bcrypt.hashpw(password.encode('utf-8'),
-                         bcrypt.gensalt())
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
