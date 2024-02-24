7247ca04a23d0f8ed241aca5e866e7b1c2a0c6f7
OUALIID <walidelhadim8@gmail.com>
2024-02-24 14:16:07 +0100

    A profile function to respond to the GET /profile path.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,15 +58,15 @@ def logout():
     abort(403)
 
 
-@app.route("/profile")
+@app.route("/profile", methods=['GET'])
 def profile():
-    """Profile function to respond to the profile path."""
+    """A profile function to respond to the GET /profile path."""
     session_id = request.cookies.get("session_id")
     if session_id:
-        user = AUTH.get_user_from_session_id("session_id")
+        user = AUTH.get_user_from_session_id(session_id)
         if user:
             return jsonify({"email": user.email}), 200
-    abort(403)
+    return jsonify({"error": "Invalid session ID"}), 403
 
 
 if __name__ == "__main__":
