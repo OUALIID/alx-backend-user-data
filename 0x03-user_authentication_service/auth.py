6d3810636124760373f14813e371d880d7fdf069
OUALIID <walidelhadim8@gmail.com>
2024-02-24 02:39:32 +0100

    A function that returns a string representation of a new UUID.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,7 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
-
+import uuid
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password using bcrypt."""
@@ -35,3 +35,7 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def _generate_uuid() -> str:
+        """A function that returns a string representation of a new UUID."""
+        return str(uuid.uuid4())
