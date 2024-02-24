65cfdc58577c450046870b791efc6e8f3f87331c
OUALIID <walidelhadim8@gmail.com>
2024-02-24 15:41:02 +0100

    Profile function to reply.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -61,11 +61,12 @@ def logout():
 @app.route('/profile', methods=['GET'])
 def profile():
     """Profile function to reply."""
+    email = request.form.get("email")
     session_id = request.cookies.get("session_id")
     if session_id:
         user = AUTH.get_user_from_session_id(session_id)
         if user:
-            return jsonify({"email": user.email}), 200
+            return jsonify({"email": email}), 200
     abort(403)
 
 
