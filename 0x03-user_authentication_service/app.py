0c7f0d2adbffef04109400cc05457fd82e5d3f13
OUALIID <walidelhadim8@gmail.com>
2024-02-24 15:00:09 +0100

    A profile function to respond to the GET /profile path.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,17 +58,15 @@ def logout():
     abort(403)
 
 
-@app.route('/profile', methods=['GET'], strict_slashes=False)
+@app.route('/profile', methods=['GET'])
 def profile():
-    """ GET /profile
-    Return:
-      - json
-    """
-    session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
-        abort(403)
-    return jsonify({"email": f"{user.email}"}), 200
+    """Profile function to reply."""
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user is None:
+            return jsonify({"email": user.email}), 200
+    abort(403)
 
 
 if __name__ == "__main__":
