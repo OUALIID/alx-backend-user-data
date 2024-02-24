7dd2a706b9379ffd696b03eab48b449e7d976bdb
OUALIID <walidelhadim8@gmail.com>
2024-02-24 15:37:28 +0100

    Update user's password using the reset token.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,3 +76,12 @@ class Auth:
             return token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str):
+        """Update user's password using the reset token."""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id, hashed_password=_hash_password(password),
+                                 reset_token=None)
+        except Exception:
+            raise ValueError
