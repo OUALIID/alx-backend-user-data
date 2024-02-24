424c45d1aac23ca05430a31d28f19a00c6311135
OUALIID <walidelhadim8@gmail.com>
2024-02-24 15:00:35 +0100

    A profile function to respond to the GET /profile path.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -64,8 +64,7 @@ def profile():
     session_id = request.cookies.get("session_id")
     if session_id:
         user = AUTH.get_user_from_session_id(session_id)
-        if user is None:
-            return jsonify({"email": user.email}), 200
+        return jsonify({"email": user.email}), 200
     abort(403)
 
 
