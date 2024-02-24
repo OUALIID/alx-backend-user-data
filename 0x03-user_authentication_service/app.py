3f980e0fffaec1e4bbcc57b814c4fa3f1b3cd815
OUALIID <walidelhadim8@gmail.com>
2024-02-24 03:31:06 +0100

    Login function to respond to the POST /sessions path.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 
 app = Flask(__name__)
@@ -24,6 +25,22 @@ def users():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+@app.route("/sessions", methods=['POST'])
+def login():
+    """Login function to respond to the POST /sessions path."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        if AUTH.valid_login(email, password):
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie("session_id", AUTH.create_session(email))
+            return response
+        else:
+            abort(401)
+    except NoResultFound:
+        abort(401)
 
 
 if __name__ == "__main__":
