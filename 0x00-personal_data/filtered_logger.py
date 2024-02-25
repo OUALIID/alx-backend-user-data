952acba284a2c3610cabf22a6556dc62377906ac
OUALIID <walidelhadim8@gmail.com>
2024-02-25 10:35:11 +0100

    A function that returns a connector to the database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -30,7 +30,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     "A function that returns a connector to the database"
     db_config = {
         "user": os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
