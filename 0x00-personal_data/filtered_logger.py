4e405969d6f5cdaeba9882a2e6b616eb153488b0
OUALIID <walidelhadim8@gmail.com>
2024-02-25 08:37:41 +0100

    Format the log record, redacting sensitive information.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -11,3 +12,21 @@ def filter_datum(fields: List[str], redaction: str,
     """A function that returns an ambiguous log message."""
     pattern = "|".join(fields)
     return re.sub(f"({pattern})=[^{separator}]+", f"\\1={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the log record, redacting sensitive information."""
+        for field in self.fields:
+            record.msg = re.sub(f"{field}=[^;]+", f"{field}={self.REDACTION}", record.msg)
+        return super().format(record)
