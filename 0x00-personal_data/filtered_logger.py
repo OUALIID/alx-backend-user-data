03d3b0a1a92be0c6d986034c1ada8b17bdbc67ed
OUALIID <walidelhadim8@gmail.com>
2024-02-25 05:50:16 +0100

    A function that returns an ambiguous log message.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,11 @@
 0. Regex-ing
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
     """A function that returns an ambiguous log message."""
     pattern = "|".join(fields)
     return re.sub(f"({pattern})=[^;]+", f"\\1={redaction}", message)
