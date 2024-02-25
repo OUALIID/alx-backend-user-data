304f7ad54b3667384d24c0ed60c18740fc4aa914
OUALIID <walidelhadim8@gmail.com>
2024-02-25 11:35:08 +0100

    try to fix my code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-0. Regex-ing
-"""
+"""just test"""
 import re
 from typing import List
 import logging
@@ -9,52 +7,55 @@ import os
 import mysql.connector
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """A function that returns an ambiguous log message."""
-    pattern = "|".join(fields)
-    return re.sub(f"({pattern})=[^{separator}]+", f"\\1={redaction}", message)
-
-
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
-def get_logger() -> logging.Logger:
-    """A function that takes no arguments
-    and returns a logging.Logger object."""
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-    formatter = RedactingFormatter(fields=PII_FIELDS)
-    logger.addHandler(logging.StreamHandler().setFormatter(formatter))
-    return logger
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """A function that returns a connector to the database."""
-    db_username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-    return mysql.connector.connect(
-        user=db_username, password=db_password, host=db_host, database=db_name
-    )
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """just test"""
+    for field in fields:
+        regex = f"{field}=[^{separator}]*"
+        message = re.sub(regex, f"{field}={redaction}", message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class"""
+    """just test"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """just test"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Format the log record, redacting sensitive information."""
-        for field in self.fields:
-            record.msg = re.sub(f"{field}=[^;]+",
-                                f"{field}={self.REDACTION}", record.msg)
-        return super().format(record)
+        """just test"""
+        org = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, org, self.SEPARATOR)
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def get_logger() -> logging.Logger:
+    """just test"""
+    log = logging.getLogger("user_data")
+    log.setLevel(logging.INFO)
+    log.propagate = False
+    sh = logging.StreamHandler()
+    sh.setFormatter(RedactingFormatter(PII_FIELDS))
+    log.addHandler(sh)
+    return log
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """just test"""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    return mysql.connector.connect(
+        user=username, password=password, host=host, database=db_name
+    )
