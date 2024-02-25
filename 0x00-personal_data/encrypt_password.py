7d8462a4c5ed94da329d03d3a83fade6723e8504
OUALIID <walidelhadim8@gmail.com>
2024-02-25 12:04:55 +0100

    Validates a password against a hashed password.

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,7 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Hashes a password using bcrypt."""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates a password against a hashed password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
