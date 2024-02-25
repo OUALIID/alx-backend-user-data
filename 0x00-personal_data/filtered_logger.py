7d06fdeaa0379790fb29fb8c3cf77d6b85f30e08
OUALIID <walidelhadim8@gmail.com>
2024-02-25 10:55:59 +0100

    A function that returns a connector to the database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,10 @@
 """
 0. Regex-ing
 """
-import re, os
+import re
 from typing import List
 import logging
+import os
 import mysql.connector
 
 
@@ -31,14 +32,14 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     "A function that returns a connector to the database"
-    db_username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-    conn = mysql.connector.connect(
-        user=db_username, password=db_password, host=db_host, database=db_name
+
+    return mysql.connector.connect(
+        user=username, password=password, host=host, database=db_name
     )
-    return conn
 
 
 class RedactingFormatter(logging.Formatter):
